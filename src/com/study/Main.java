feb02914a315d8a8d84b0fe144d36166662f43fe
wanghuiwu123 <wang123-hi@163.com>
2018-10-30 10:42:01 +0800

    添加说明

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -13,6 +13,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(DateUtil.getCurenntTime());
+        System.out.println(DateUtil.getCurenntTime()+"当前日期");
     }
 }
