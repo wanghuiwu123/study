e27abf15c00f6f03d760181b2f93cae0edc21ed3
wanghuiwu123 <wang123-hi@163.com>
2018-10-30 09:53:36 +0800

    初始化导入

---
+++ src/com/study/Main.java
@@ -0,0 +1,13 @@
+package com.study;
+
+/**
+ * 测试工具类
+ * @author whw
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(System.currentTimeMillis());
+    }
+}
