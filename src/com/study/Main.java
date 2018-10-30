29439f1fcf57bf6ade6cdaca080170a1f733ab0f
wanghuiwu123 <wang123-hi@163.com>
2018-10-30 10:18:57 +0800

    日期精确到天

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -11,7 +11,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         System.out.println(sdf.format(new Date()));
     }
 }
