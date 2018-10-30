ea8e8076d2d54e92b2db56f400dc8a322296f844
wanghuiwu123 <wang123-hi@163.com>
2018-10-30 10:26:12 +0800

    日期单独声明

--- src/com/study/Main.java
+++ src/com/study/Main.java
@@ -12,6 +12,7 @@ public class Main
     public static void main(String[] args)
     {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(sdf.format(new Date()));
+        Date date = new Date();
+        System.out.println(sdf.format(date));
     }
 }
